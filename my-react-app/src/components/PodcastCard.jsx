f6597fb759c397583ab03930dcf78691267c1297
Charme-Lee <charmelishivha2@gmail.com>
2025-10-24 17:30:26 +0200

    Added seasons count, date formater and genre names

--- my-react-app/src/components/PodcastCard.jsx
+++ my-react-app/src/components/PodcastCard.jsx
@@ -2,5 +2,24 @@ import react from "react";
 import { GenreService } from "../utils/GenreService";
 import { DateUtils } from "../utils/Dateutils";
 
+const PodcastCard = ({ podcast, onSelect, genres }) => {
+  const genreNames = GenreService.getNames(podcast.genre || [], genres || []); // Uses genres prop
+  const updatedDate = DateUtils.format(
+    podcast.updated || new Date().toISOString()
+  );
+  console.log( "Podcast in Card:", podcast );
+  
+  let seasonCount =podcast.seasons && Array.isArray(poddcast.seasons) ? (typeof podcast.seasons === 'number' ? podcast.seasons : 0);
 
-const PodcastCard =()+
+  return (
+    <div>
+      
+    </div>
+  )
+
+
+
+
+};
+
+export default PodcastCard;
