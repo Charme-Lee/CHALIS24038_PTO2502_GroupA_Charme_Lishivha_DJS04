9411081d27d380adc92fbe49293dd137ee8564af
Charme-Lee <charmelishivha2@gmail.com>
2025-10-24 16:30:52 +0200

    add utilities date formatter and genre ID converter

---
+++ my-react-app/src/utils/GenreService.js
@@ -0,0 +1,9 @@
+import { genres } from "../views/data";
+
+export const GenreService = {
+  getNames(genreIds, genres) {
+    return genreIds.map(
+      (id) => genres.find((g) => g.id === id)?.title || "Unknown"
+    );
+  },
+};
