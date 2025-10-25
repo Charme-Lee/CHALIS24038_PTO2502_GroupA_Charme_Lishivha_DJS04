ca8f562c6bb0075027f8930a91dfc29521743f18
Charme-Lee <charmelishivha2@gmail.com>
2025-10-25 11:00:04 +0200

    Add JSDoc comments & imports

---
+++ my-react-app/src/components/SearchBar.jsx
@@ -0,0 +1,15 @@
+/** @jsxImportSource react */
+import React from "react";
+
+/**
+ * A component for searching and filtering podcasts with pagination controls.
+ * @param {Object} props - The component props.
+ * @param {Array<Object>} props.podcasts - The list of all podcasts.
+ * @param {function} props.onPodcastSelect - Callback to select a podcast.
+ * @param {Array<Object>} props.genres - The list of available genres.
+ * @param {function} props.setCurrentPodcasts - Callback to update the current podcasts list.
+ * @param {number} props.currentPage - The current page number.
+ * @param {function} props.setCurrentPage - Callback to update the current page.
+ * @param {number} props.itemsPerPage - The number of items per page.
+ * @returns {JSX.Element} The rendered search bar component.
+ */
